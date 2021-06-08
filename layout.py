5eefe65712655ac5639ccd154637d6b0e785bbb8
Chris Roeseler <ch.roeseler@gmail.com>
2021-06-08 21:22:26 +0100

    Started to add layout orientation

--- layout.py
+++ layout.py
@@ -37,39 +37,73 @@ def matrix_init():
           → ┗┛┗┛┗┛┗┛→↑
 """
 
+    #Din unten links
     led_id = 0
-    matrix = dict()
+    unl = dict()
     for i in range(5):
-        matrix[(0,i)] = led_id
+        unl[(0,i)] = led_id
         led_id += 1
     for i in range(4,-1,-1):
-        matrix[(1,i)] = led_id
+        unl[(1,i)] = led_id
         led_id += 1
     for i in range(5):
-        matrix[(2,i)] = led_id
+        unl[(2,i)] = led_id
         led_id += 1
     for i in range(4,-1,-1):
-        matrix[(3,i)] = led_id
+        unl[(3,i)] = led_id
         led_id += 1
 
+    #Din oben links
+    led_id = 0
+    obl = dict()
+    for i in range(4,-1,-1):
+        obl[(0,i)] = led_id
+        led_id += 1
+    for i in range(5):
+        obl[(1,i)] = led_id
+        led_id += 1
+    for i in range(4,-1,-1):
+        obl[(2,i)] = led_id
+        led_id += 1
+    for i in range(5):
+        obl[(3,i)] = led_id
+        led_id += 1
+
+    #Din unten rechts
     led_id = 19
-    invers = dict()
+    unr = dict()
     for i in range(5):
-        invers[(0,i)] = led_id
+        unr[(0,i)] = led_id
         led_id -= 1
     for i in range(4,-1,-1):
-        invers[(1,i)] = led_id
+        unr[(1,i)] = led_id
         led_id -= 1
     for i in range(5):
-        invers[(2,i)] = led_id
+        unr[(2,i)] = led_id
         led_id -= 1
     for i in range(4,-1,-1):
-        invers[(3,i)] = led_id
+        unr[(3,i)] = led_id
         led_id -= 1
 
-    return (matrix, invers)
+    #Din oben rechts
+    led_id = 19
+    obr = dict()
+    for i in range(4,-1,-1):
+        obr[(0,i)] = led_id
+        led_id -= 1
+    for i in range(5):
+        obr[(1,i)] = led_id
+        led_id -= 1
+    for i in range(4,-1,-1):
+        obr[(2,i)] = led_id
+        led_id -= 1
+    for i in range(5):
+        obr[(3,i)] = led_id
+        led_id -= 1
 
-def full_layout(x_boxes, y_boxes, inverse=False):
+    return (obl,unl,obr,unr)
+
+def full_layout(x_boxes, y_boxes, inverse=False, unten=False, rechts=False):
     """ x_boxes: Number of Beer Crates horizontal
         y_boxes: Number of Beer Crates vertical"""
     matrix = matrix_init()
@@ -78,7 +112,7 @@ def full_layout(x_boxes, y_boxes, inverse=False):
     if not inverse:
         for y in range(y_boxes):
             for x in range(x_boxes):
-                for k,v in matrix[0].items():
+                for k,v in matrix[1].items():
                     coordinate = (k[0] + (4*x), k[1] + (5*y))
                     layout[coordinate] = v + (20*box_count)
                 box_count += 1
@@ -87,14 +121,14 @@ def full_layout(x_boxes, y_boxes, inverse=False):
             if y & 1:
                 #'odd --> use invers
                 for x in range(x_boxes-1, -1, -1):
-                    for k,v in matrix[1].items():
+                    for k,v in matrix[3].items():
                         coordinate = (k[0] + (4*x), k[1] + (5*y))
                         layout[coordinate] = v + (20*box_count)
                     box_count += 1
             else:
                 #'even --> use matrix
                 for x in range(x_boxes):
-                    for k,v in matrix[0].items():
+                    for k,v in matrix[1].items():
                         coordinate = (k[0] + (4*x), k[1] + (5*y))
                         layout[coordinate] = v + (20*box_count)
                     box_count += 1
