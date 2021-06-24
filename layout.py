a1ff368474b84afc8baf189f74e2d9e0a04dfeea
Chris Roeseler <ch.roeseler@gmail.com>
2021-06-25 01:19:54 +0200

    refactor: lint lint lint lint

--- layout.py
+++ layout.py
@@ -3,11 +3,11 @@ import numpy as np
 def full_layout(x_boxes, y_boxes, vert=False, horz=False):
     """ x_boxes: Number of Beer Crates horizontal
         y_boxes: Number of Beer Crates vertical"""
-    layout = matrix =  np.array([[0, 9, 10, 19],
-                        [1, 8, 11, 18],
-                        [2, 7, 12, 17],
-                        [3, 6, 13, 16],
-                        [4, 5, 14, 15]])
+    layout = matrix = np.array([[0, 9, 10, 19],
+                                [1, 8, 11, 18],
+                                [2, 7, 12, 17],
+                                [3, 6, 13, 16],
+                                [4, 5, 14, 15]])
     for x in range(1, x_boxes):
         layout = np.concatenate((layout, matrix+x*20), axis=1)
     for y in range(1, y_boxes):
@@ -18,7 +18,3 @@ def full_layout(x_boxes, y_boxes, vert=False, horz=False):
     if horz:
         layout = np.flipud(layout)
     return layout
-
-
-if __name__ == '__main__':
-    matrix = full_layout(5,3)
