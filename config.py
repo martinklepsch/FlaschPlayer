c59298bc5332a066d01f5ce7be20c51b561f3097
Martin Klepsch <martinklepsch@googlemail.com>
2021-06-25 12:22:59 +0200

    waiting_line ist im config ordner

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 work_dir = os.environ['WORK_DIR']
 
-waiting_line = work_dir + "/waiting_line"
+waiting_line = work_dir + "/config/waiting_line"
 
-waiting_line_lock = work_dir + "/waiting_line.lock"
+waiting_line_lock = work_dir + "/config/waiting_line.lock"
